ff35983a51263e261589f81f2f74115d6ef9dfe7
Dioni Mercado <me@dionimercado.com>
2019-08-08 21:16:56 -0400

    Initial commit. Coming soon message

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Guira y Tambora - Boston</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Luckiest+Guy&display=swap" rel="stylesheet">
+
+  <style media="screen">
+    body {
+      height: 100vh;
+    }
+    img {
+      max-width: 600px;
+      width: 90%;
+    }
+    h2 {
+      font-family: 'Luckiest Guy', cursive;
+      background-color: #603814;
+      color: #fff;
+      padding: 10px 20px 1px;
+      text-transform: uppercase;
+      letter-spacing: 1px;
+    }
+  </style>
+</head>
+<body>
+  <div class="d-flex flex-column justify-content-center align-items-center h-100 w-100">
+    <img src="<?php echo get_template_directory_uri() ?>/assets/images/logo.png" alt="">
+    <h2 class="mt-5">Coming Soon!</h2>
+  </div>
+</body>
+</html>
